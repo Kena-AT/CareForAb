c4e52dc00460707be351e1afd86227cd92655eb2
Kena-AT <Kenaararso4@gmail.com>
2026-03-25 23:13:38 +0300

    feat: Fixing CI lint error

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,22 @@
-export default [
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off"
     }
   }
 ];
+
+export default eslintConfig;
